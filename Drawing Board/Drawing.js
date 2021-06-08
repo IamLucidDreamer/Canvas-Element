15acf700d4b207b8015209186a5eeaf2fedae043
Lucid Dreamer <shuklamanasofficial@gmail.com>
2021-06-08 10:29:28 +0530

    Drawing Board Updated

--- Drawing Board/Drawing.js
+++ Drawing Board/Drawing.js
@@ -2,60 +2,53 @@ var paintcanvas = document.getElementById("canvas1");
 var context = paintcanvas.getContext("2d");
 var color = 'black';
 var radius = 50;
-// only paint if mouse is  being dragged (moved while the button is pressed)
 var isPainting = false;
-function setWidth (value)
-{
-  var c1 = document.getElementById("canvas1");
-   if (isNumeric(value))
-   {
-     c1.width = value;
-   }
-}
-function setHeight(value)
-{
-  var c2 = document.getElementById("canvas1");
-  if (isNumeric(value))
-  {
-    c2.height = value;
-  }
-}
-function clearCanvas ()
-{
-  context.clearRect(0, 0, paintcanvas.width, paintcanvas.height);
-}
-function paintCircle (x, y)
-{
-  // make sure to start a new circle each time
-  context.beginPath();
-  // draw circle using a complete (2*PI) arc around given point
-  context.arc(x, y, radius, 0, Math.PI * 2, true);
-  context.fillStyle = color;
-  context.fill();
-}
-// verify the given value is actually a number
-function isNumeric (value)
-{
-  // standard JavaScript function to determine whether a string is an illegal number (Not-a-Number)
-  return !isNaN(value);
+
+function setWidth (value) {
+  var canvas = document.getElementById("canvas1");
+  canvas.width = value;
 }
 
-function startPaint()
-{
- var isPainting = new Boolean(true);
- return isPainting;
+function setHeight(value){
+  var canvas = document.getElementById("canvas1");
+  canvas.height = value;
 }
 
- function endPaint()
-{
-  var isPainting = new Boolean(false);
-  return isPainting;
+function startPaint(x,y){
+  isPainting = true;
+  doPaint(x,y);
 }
 
-function doPaint(x , y)
-{
-  if (startPaint() == true)
+function endPaint(){
+  isPainting = false;
+}
+
+function doPaint(x,y){
+  if (isPainting == true)
     {
-      paintCircle(x , y);
+      paintCircle(x,y);
     }
+}
+
+function changeColor(newColor){
+  document.getElementById("brush").value = newColor;
+}
+
+function resizeBrush(newSize){
+  radius = newSize;
+  var x = document.getElementById("otp");
+  x.value = newSize;
+}
+
+function clearCanvas () {
+    context.clearRect(0, 0, paintcanvas.width, paintcanvas.height);
+}
+
+function paintCircle (x, y,color) {
+    // make sure to start a new circle each time
+    context.beginPath();
+    // draw circle using a complete (2*PI) arc around given point
+    context.arc(x, y, radius, 0, Math.PI * 2, true);
+    context.fillStyle = document.getElementById("brush").value;
+    context.fill();
 }
